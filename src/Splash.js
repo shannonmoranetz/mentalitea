3377184b67a0a96a1073a2c6eb8a5e1e897e6789
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2018-12-18 15:03:58 -0700

    setup

---
+++ src/Splash.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles/Main.scss';
+
+export default class Splash extends Component {
+  render() {
+    return (
+     
+    );
+  }
+}
---
+++ src/TeaList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './styles/Main.scss';
+
+export default function TeaList() {
+    return (
+     
+    );
+}
