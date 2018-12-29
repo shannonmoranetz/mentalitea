b0c5c8739f2ad21865015783571afc2a4ea39500
shannonmoranetz <shannonrmoranetz@gmail.com>
2018-12-29 15:44:16 -0700

    Add initial tea card styling and position

--- src/TeaCard.js
+++ src/TeaCard.js
@@ -19,23 +19,37 @@ export default class TeaCard extends Component {
     if(this.state.toggleExpand) {
     return (
         <li className="tea-card" onClick={this.toggleExpandFunction}>
-          <h2 className="tea-name">{this.props.filteredTeaList.tea}</h2>
-          <img className="tea-image" src={require(`./styles/images/card-photos/tea-card-${this.props.id}.png`)} alt=""/>
+          <h2 className="tea-name-unexpanded">{this.props.filteredTeaList.tea}</h2>
+          <img className="tea-image-unexpanded" src={require(`./styles/images/card-photos/tea-card-${this.props.id}.png`)} alt=""/>
         </li>
     )} else {
       return (
           <li  className="full-tea-info" onClick={this.toggleExpandFunction}>
-            <h1>{this.props.filteredTeaList.tea}</h1> 
-            <h2>{this.props.filteredTeaList.category}</h2>
-              <h3>{this.props.filteredTeaList.caffeine}</h3>
-              {
-                this.props.filteredTeaList.tastes.map((taste, index) => {
-                  return (
-                    <p key={index} value={taste}>{taste}</p>
-                  )
-                })
-              }
-            <a href={this.props.filteredTeaList.link} target="_blank" rel="noopener noreferrer">Buy Now</a>
+            <h1 className="tea-name-expanded">❁ {this.props.filteredTeaList.tea} ❁</h1> 
+            <div className="tea-info-container">
+              <div className="category-container">
+                <h3 className="info-subheading">Category:</h3>
+                <h2 className="tea-category">{this.props.filteredTeaList.category}</h2>
+              </div>
+              <div className="caffeine-container">
+                <h3 className="info-subheading">Caffeine Level:</h3>
+                <h3 className="tea-caffeine">{this.props.filteredTeaList.caffeine}</h3>
+              </div>
+              <div className="tastes-container">
+                <h3 className="info-subheading">Tastes included:</h3>
+                {
+                  this.props.filteredTeaList.tastes.map((taste, index) => {
+                    return (
+                      <p className="tea-tastes" key={index} value={taste}>✿  {taste}</p>
+                      )
+                    })
+                  }
+              </div>
+              <div className="image-link-container">
+                <img className="tea-image-expanded" src={require(`./styles/images/card-photos/tea-card-${this.props.id}.png`)} alt=""/>
+                <a className="buy-link" href={this.props.filteredTeaList.link} target="_blank" rel="noopener noreferrer">Buy Now</a>
+              </div>
+            </div>
           </li>
       )
     }
