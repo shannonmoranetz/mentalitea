917fa849310277d1920d8ae2e98aa00a6db388c4
shannonmoranetz <shannonrmoranetz@gmail.com>
2018-12-28 19:25:43 -0700

    Add initial tea card styling

--- src/TeaCard.js
+++ src/TeaCard.js
@@ -18,15 +18,15 @@ export default class TeaCard extends Component {
   render() {
     if(this.state.toggleExpand) {
     return (
-        <li onClick={this.toggleExpandFunction}>
-          <p className="tea-name">{this.props.filteredTeaList.tea}</p>
+        <li className="tea-card" onClick={this.toggleExpandFunction}>
+          <h2 className="tea-name">{this.props.filteredTeaList.tea}</h2>
         </li>
     )} else {
       return (
           <li  className="full-tea-info" onClick={this.toggleExpandFunction}>
             <h1>{this.props.filteredTeaList.tea}</h1> 
             <h2>{this.props.filteredTeaList.category}</h2>
-              <p>{this.props.filteredTeaList.caffeine}</p>
+              <h3>{this.props.filteredTeaList.caffeine}</h3>
               {
                 this.props.filteredTeaList.tastes.map((taste, index) => {
                   return (
