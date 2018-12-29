3e82e988bac31c51a995d61d0ef513a8959e74da
shannonmoranetz <shannonrmoranetz@gmail.com>
2018-12-28 19:10:24 -0700

    Add tea list styling

--- src/TeaList.js
+++ src/TeaList.js
@@ -5,7 +5,9 @@ import TeaCard from './TeaCard.js';
 export default function TeaList(props) {
     return (
      <div className="tea-list">
-        <h1>Teas for when you're feeling <span className="selected-descriptor">{props.userSelectedMood}</span>:</h1>
+        <div className="tealist-heading-container">
+            <h1 className="tealist-heading">Teas for when you're feeling <span className="selected-descriptor">{props.userSelectedMood}</span>:</h1>
+        </div>
         <ul>
         {
             props.userSelectedTea.map((tea, index) => {
